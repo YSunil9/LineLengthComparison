3633bcbeb4ed194deda5e60150c3269f9333110a
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-11 19:56:13 +0530

    [SUNIL]:Compare The Lines Using Equals Method

--- LineLengthComparision/Program.cs
+++ LineLengthComparision/Program.cs
@@ -7,7 +7,7 @@
             double[] length = new double[2];
             for (int i = 0; i < 2; i++)
             {
-                Console.WriteLine("Welcome to Line Comparison Computation Program");
+                Console.WriteLine("Welcome to Line Comparison Computation Program\n");
                 int x1, y1;
                 Console.WriteLine("Enter the 1st co-ordinates x1");
                 x1 = Convert.ToInt32(Console.ReadLine());
@@ -19,18 +19,19 @@
                 Console.WriteLine("Enter the 1st co-ordinates y2");
                 y2 = Convert.ToInt32(Console.ReadLine());
                 length[i] = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+                Console.WriteLine(length[i] + " is the length of this line\n");
             }
-            if (length[0].CompareTo(length[1]) > 0)
+            if (length[0].Equals(length[1]))
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else if (length[0] > length[1])
             {
                 Console.WriteLine("Line 01 is greater than line 02");
             }
-            else if (length[0].CompareTo(length[1]) < 0)
-            {
-                Console.WriteLine("Line 02 is greater than line 01");
-            }
             else
             {
-                Console.WriteLine("Both Lines Are Equal");
+                Console.WriteLine("Line 02 is greater than line 01");
             }
         }
     }
